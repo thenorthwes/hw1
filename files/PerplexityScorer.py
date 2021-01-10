2b09eb70aa0f6d12432f8031aa95679c3808bfcd
Wes Saunders <thenorthwesley@gmail.com>
2021-01-10 11:36:40 -0800

    Code clean up -- refactors only -- no true change

--- files/PerplexityScorer.py
+++ files/PerplexityScorer.py
@@ -9,27 +9,24 @@ from math import log2
 from unigrammodel import UNK_, STOP_
 
 
-def calculate_perplexity(evalPath: str, probs: dict) -> int:
+def calculate_perplexity(eval_path: str, probs: dict) -> int:
     perplexity = -1
     exponent = 0
-    eval_stream = open(evalPath, "r")
+    eval_stream = open(eval_path, "r")
     sentence = eval_stream.readline()
-    corpusSize = 0
+    corpus_size = 0
     while sentence:
         for token in sentence.split():
             exponent += log2(probs.get(token, probs[UNK_]))
-            corpusSize += 1
+            corpus_size += 1
             sentence = eval_stream.readline()
     # Add stop a single time
-    corpusSize += 1
+    corpus_size += 1
     exponent += log2(probs[STOP_])
 
-
-    # Perplexity is equal to 2 to the power of the negative `l`
-    perplexity = 2 ** -(exponent/corpusSize)
-
-    # for validationword in validationGrams.getUnigrams().keys():
-    #     perplexity
+    #  Perplexity is equal to 2 to the power of the negative `l`
+    perplexity = 2 ** -(exponent / corpus_size)
 
     print("Unigram Perplexity Score: {}".format(perplexity))
+    eval_stream.close()
     return perplexity
--- unigrammodel.py
+++ unigrammodel.py
@@ -12,75 +12,75 @@ def pad_sentence(sentence: str):
 
 class Unigrams:
     def __init__(self, trainingDataPath: str) -> None:
-        self.unigramcounter: dict  # set up when ready
+        self.unigram_counter: dict = None  # set up when ready
         self.probabilities: dict
-        self.vocabsize: int = 0  # assume empty vocab size to start
-        self.train(trainingDataPath)
-        self.reportTraining()
+        self.vocab_size: int = 0  # assume empty vocab size to start
+        self.extract_vocab(trainingDataPath)
+        self.report_training()
 
     # main worker for unigram model training
-    def train(self, trainingDataPath):
+    def extract_vocab(self, training_data_path):
 
         # set up our model structure
-        self.unigramcounter = {}  # use a dictionary for convenience
+        self.unigram_counter = {}  # use a dictionary for convenience
 
-        trainingDataFile = open(trainingDataPath, "r")  # open a handle to our training corpus (read mode)
-        sentence = trainingDataFile.readline()  # get the first sentence
+        training_data_file = open(training_data_path, "r")  # open a handle to our training corpus (read mode)
+        sentence = training_data_file.readline()  # get the first sentence
 
         while sentence:
             sentence = pad_sentence(sentence)
-            self.train_sentence(sentence)  # update our model with this sentence
-            sentence = trainingDataFile.readline()  # go to the next line
-        trainingDataFile.close()
+            self.consume_sentence(sentence)  # update our model with this sentence
+            sentence = training_data_file.readline()  # go to the next line
+        training_data_file.close()
 
     '''
     Train structures on a sentence -- takes a tokenized sentence (white space indicates a new token)
     and process into our unigram model
     '''
 
-    def train_sentence(self, sentence):
+    def consume_sentence(self, sentence):
         for token in sentence.split():
             # get a token and if we are first meeting it, default to 0
-            self.unigramcounter[token] = self.unigramcounter.get(token,0) + 1
-            self.vocabsize += 1
+            self.unigram_counter[token] = self.unigram_counter.get(token, 0) + 1
+            self.vocab_size += 1
 
-    def reportTraining(self):
-        print("Number of unigrams: {}".format(len(self.unigramcounter.keys())))
-        print("Vocabulary Size: {}", self.vocabsize)
-        print("Number of occurrences of 'the': {}".format(self.unigramcounter["the"]))
-        print("Number of occurrences of '.': {}".format(self.unigramcounter["."]))
-        print("Number of occurrences of special stop: {}".format(self.unigramcounter[("%s" % STOP_)]))
+    def report_training(self):
+        print("Number of unigrams: {}".format(len(self.unigram_counter.keys())))
+        print("Vocabulary Size: {}", self.vocab_size)
+        print("Number of occurrences of 'the': {}".format(self.unigram_counter["the"]))
+        print("Number of occurrences of '.': {}".format(self.unigram_counter["."]))
+        print("Number of occurrences of special stop: {}".format(self.unigram_counter[("%s" % STOP_)]))
 
-    def getUnigrams(self):
-        return self.unigramcounter
+    def get_unigrams(self):
+        return self.unigram_counter
 
-    def getVocabSize(self):
-        return self.vocabsize
+    def get_vocab_size(self):
+        return self.vocab_size
 
 class UnigramModel:
     def __init__(self, trainingDataPath: str) -> None:
-        self.probabilities: dict
+        self.probabilities: dict = None  # set up later
         self.unigrams = Unigrams(trainingDataPath)
         self.learn()
-        self.reportLearning()
+        self.report_learning()
 
     def learn(self):
         # probability is the number of encounters / total tokens
         # Get our Unigrams and Vocab
-        unigrams = self.unigrams.getUnigrams()
-        vocabsize = self.unigrams.getVocabSize()
+        unigrams = self.unigrams.get_unigrams()
+        vocab_size = self.unigrams.get_vocab_size()
         # add the UNK place holder -- start at 0 occurrences
         unigrams[UNK_] = 0
 
         self.probabilities = {}
 
         for unigram in unigrams.keys():
-            self.probabilities[unigram] = unigrams[unigram] / vocabsize
+            self.probabilities[unigram] = unigrams[unigram] / vocab_size
             if unigrams[unigram] <= 1:  # [unk] the single occurrence unigrams
                 unigrams[UNK_] = unigrams.get(UNK_, 0) + unigrams[unigram]
-        self.probabilities[UNK_] = unigrams[UNK_] / vocabsize
+        self.probabilities[UNK_] = unigrams[UNK_] / vocab_size
 
-    def reportLearning(self):
+    def report_learning(self):
         print("Probability of 'the': {}".format(self.probabilities["the"]))
         print("Probability of [unk]: {}".format(self.probabilities[UNK_]))
 
